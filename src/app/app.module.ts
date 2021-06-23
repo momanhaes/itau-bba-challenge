c1629e2190eded201d1ec5e3e020f0ba3640cf2f
Matheus Manhães <momanhaes@gmail.com>
2021-06-23 02:21:11 -0300

    feat: Insere primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
 import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login/login.component';
+import { HomePageComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { WindowService } from './services/window.service';
+
+import { ResizeDirective } from './directives/resize.directive';
+import { ROUTES } from './app.routes';
+
+const SERVICES = [WindowService];
+
+const DECLARATIONS = [
+  AppComponent,
+  ResizeDirective,
+  LoginPageComponent,
+  HomePageComponent,
+  HeaderComponent,
+  FooterComponent,
+];
+
+const ANGULAR_MODULES = [
+  RouterModule.forRoot(ROUTES),
+  BrowserModule,
+  BrowserAnimationsModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  FormsModule,
+];
+
+const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [...DECLARATIONS],
+  imports: [...ANGULAR_MODULES, ...MATERIAL_MODULES],
+  providers: [...SERVICES],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login/login.component';
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: HomePageComponent },
 ];
